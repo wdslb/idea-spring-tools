1b74facccf69cd929aded4df78cba80ad3f7d1b8
wdlth <wdlth@wdlth.com>
2021-08-27 22:36:39 +0800

    Update 2021-08-27
    1. Update gradle settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement{
     repositories {
+        gradlePluginPortal()
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/public")
-        gradlePluginPortal()
     }
 }
 
